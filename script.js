ab409d16e137717fe8fca78c95555796dcccf739
André Gabriel <andrespop994@gmail.com>
2022-10-25 21:31:28 -0600

    Proyecto 1 Level Up

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var objet = {name :"andy", age : 25, phone : 1245};
+ 
+
+console.log(objet);
+
+console.log(Object.keys(objet));
+console.log(Object.values(objet));
+console.log(Object.entries(objet));
